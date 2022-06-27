143e82951af7a13bbdb1f8a3d690973b39af83db
Yeojin <hyj01622@naver.com>
2022-06-27 23:08:15 +0900

    Refector

--- src/index.js
+++ src/index.js
@@ -4,15 +4,28 @@ const plus = document.querySelector('.plus');
 const minus = document.querySelector('.minus');
 const num = document.querySelector('.num');
 
+const ADD = 'Add';
+const SUBT = 'Subt';
+
 num.innerText = 0;
 
 const countModifier = (count = 0, action) => {
-  if (action.type === 'Add') {
-    return count + 1;
-  } else if (action.type === 'Subt') {
-    return count - 1;
-  } else {
-    return count;
+  // if (action.type === 'Add') {
+  //   return count + 1;
+  // } else if (action.type === 'Subt') {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+
+    case SUBT:
+      return count - 1;
+
+    default:
+      return count;
   }
 };
 
@@ -23,11 +36,11 @@ const onChange = () => {
 };
 
 const handleAdd = () => {
-  countStore.dispatch({ type: 'Add' });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleSubt = () => {
-  countStore.dispatch({ type: 'Subt' });
+  countStore.dispatch({ type: SUBT });
 };
 
 countStore.subscribe(onChange);
